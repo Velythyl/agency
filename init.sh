69cbc6c1da299be2a2b9f16463e27902e11861be
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2020-04-28 22:27:28 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ apt-get update -y && apt-get upgrade -y
 apt-get install git -y
 apt-get install dkms -y
 apt-get install build-essential -y
-apt install curl
-apt install python3-pip
-apt install python2
+apt install curl -y
+apt install python3-pip -y
+apt install python2 -y
